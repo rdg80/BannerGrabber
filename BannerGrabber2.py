d7ab6839ba260237b728c84e5b716301add820b1
rdg80 <53378065+rdg80@users.noreply.github.com>
2024-09-18 18:38:58 -0700

    Update BannerGrabber2.py

--- BannerGrabber2.py
+++ BannerGrabber2.py
@@ -2,9 +2,11 @@
 
 import socket
 
+#setting timeout = s.settimeout() in seconds - will timeout if no response in amount of seconds specified
 def banner(ip, port):
     s = socket.socket()
     s.connect((ip, int(port)))
+    s.settimeout(5)
     print(s.recv(1024))
 
 def main():
